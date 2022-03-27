e6cd9ac462b162789e4af12add41c99ea404a407
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-03-27 13:34:06 +0200

    Changed the alias to transfer the amqp auth to a different service

--- api/security/__init__.py
+++ api/security/__init__.py
@@ -14,13 +14,3 @@ _logger = logging.getLogger('REST-API.security')
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/oauth/token")
-
-
-def request_is_authorized(request: Request, request_id: str) -> str:
-    """Check if the headers contain a valid bearer token
-    
-    :param request: The request
-    :param request_id: The request id which is associated with this request
-    :return: True if the token is valid
-    """
-    
