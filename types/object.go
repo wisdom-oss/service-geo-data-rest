7f1ceb6dd1cedf61d500ff47b0d68fc920c13ceb
Jan Eike Suchard <jan@suchard.cloud>
2024-05-29 16:27:03 +0200

    creating types for single layers and object

---
+++ types/object.go
@@ -0,0 +1,35 @@
+package types
+
+import (
+	"encoding/json"
+
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/twpayne/go-geom"
+	"github.com/twpayne/go-geom/encoding/geojson"
+)
+
+type Object struct {
+	ID                   pgtype.Int8            `json:"id" db:"id"`
+	Geometry             geom.T                 `json:"geometry" db:"geometry"`
+	Name                 pgtype.Text            `json:"name" db:"name"`
+	Key                  pgtype.Text            `json:"key" db:"key"`
+	AdditionalProperties map[string]interface{} `json:"additionalProperties" db:"additional_properties"`
+}
+
+func (o Object) MarshalJSON() ([]byte, error) {
+	type obj struct {
+		ID                   pgtype.Int8            `json:"id" db:"id"`
+		Geometry             json.RawMessage        `json:"geometry" db:"geometry"`
+		Name                 pgtype.Text            `json:"name" db:"name"`
+		Key                  pgtype.Text            `json:"key" db:"key"`
+		AdditionalProperties map[string]interface{} `json:"additionalProperties" db:"additional_properties"`
+	}
+	out := obj{
+		ID:                   o.ID,
+		Name:                 o.Name,
+		Key:                  o.Key,
+		AdditionalProperties: o.AdditionalProperties,
+	}
+	out.Geometry, _ = geojson.Marshal(o.Geometry)
+	return json.Marshal(out)
+}
