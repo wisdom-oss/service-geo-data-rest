1886083af621ae756b797d7b187f66822d878b10
Jan Eike Suchard <jan@suchard.cloud>
2024-06-17 15:18:16 +0200

    adding queries to create new layers

--- resources/queries.sql
+++ resources/queries.sql
@@ -5,8 +5,28 @@ FROM geodata.layers;
 -- name: get-layer
 SELECT *
 FROM geodata.layers
-WHERE id = $1;
+WHERE
+    id = $1;
 
 -- name: get-layer-contents
-SELECT id, st_transform(geometry, 4326) as geometry, key, name, additional_properties
-FROM geodata.%s ;
+SELECT id, st_transform(geometry, 4326) AS geometry, key, name, additional_properties
+FROM geodata.%s;
+
+-- name: crate-layer-definition
+INSERT INTO geodata.layers(name, description, "table", crs)
+VALUES ($1, $2, $3, $4)
+RETURNING id, name, description, "table", crs;
+
+-- name: create-layer-table
+CREATE TABLE geodata.%s
+(
+    id                    bigserial PRIMARY KEY NOT NULL,
+    geometry              geometry              NOT NULL,
+    key                   text                  NOT NULL,
+    name                  text                  NOT NULL,
+    additional_properties jsonb
+);
+
+-- name: insert-shape-object
+INSERT INTO geodata.%s(geometry, key, name, additional_properties)
+VALUES ($1, $2, $3, $4);
